f5a025eb65c3a7e22d6eaf17366077e7f911cb9b
real_fan <121934188+GithubRealFan@users.noreply.github.com>
2023-06-14 20:11:37 -0400

    add run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+for no in $(seq 1 25); 
+    do btcli register --subtensor.network finney --netuid 1 --wallet.name WALLETNAME --wallet.hotkey $no --cuda.TPB 512 --cuda.update_interval 800000 --cuda.dev_id 0 1 2 3 4 5 6 7 --cuda.use_cuda  --logging.debug --no_prompt; 
+done
