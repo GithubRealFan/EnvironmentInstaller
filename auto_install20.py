be20cce466be7e13c2011b5e6cce4a23eb056bcd
real_fan <121934188+GithubRealFan@users.noreply.github.com>
2023-06-15 11:14:16 -0400

    return

--- auto_install20.py
+++ auto_install20.py
@@ -8,26 +8,6 @@ portnumber = '4200'
 mypassword = 'endless1234'
 rebootwaittime = 600
 
-def create_directory_on_partition(ssh, partition, directory_name, password=None):
-    command = f'sudo -S mkdir -p {partition}/{directory_name}'
-    print(command)
-    stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
-
-    if password is not None:
-        stdin.write(password + '\n')
-        stdin.flush()
-
-    while not stdout.channel.exit_status_ready():
-        if stdout.channel.recv_ready():
-            output = stdout.channel.recv(1024).decode('utf-8')
-            print(output, end='')
-
-        if stderr.channel.recv_stderr_ready():
-            error = stderr.channel.recv_stderr(1024).decode('utf-8')
-            print(error, end='')
-
-    print('\n')
-    return f'{partition}/{directory_name}'
 
 def execute_command(ssh, command, password=None, temp_dir=None):
     if temp_dir is not None:
@@ -79,19 +59,16 @@ if __name__ == '__main__':
     mypassword = input("Password : ")
 
     ssh = connect_ssh(myip, int(portnumber), myusername, mypassword)
-    temp_dir = create_directory_on_partition(ssh, "/dev/nvme0n1p3", "temp", mypassword)
-
 #install drivers
     ssh.connect(myip, port = int(portnumber), username=myusername, password=mypassword)
     execute_command(ssh, 'sudo -S apt -y update', mypassword)
     execute_command(ssh, 'sudo -S apt -y upgrade', mypassword)
-    execute_command(ssh, 'sudo -S apt install nvidia-driver-470', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S apt install nvidia-driver-470', mypassword)
     execute_command(ssh, 'sudo -S reboot', mypassword)
     ssh = connect_ssh(myip, int(portnumber), myusername, mypassword)
-    temp_dir = create_directory_on_partition(ssh, "/dev/nvme0n1p3", "temp", mypassword)
  
     execute_command(ssh, 'sudo -S wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin', mypassword)
-    execute_command(ssh, 'sudo -S mv cuda-ubuntu2004.pin /etc/apt/preferences.duda-repository-pin-600', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S mv cuda-ubuntu2004.pin /etc/apt/preferences.duda-repository-pin-600', mypassword)
 
     file_url="https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb"
     file_name="cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb"
@@ -100,18 +77,17 @@ if __name__ == '__main__':
     execute_command(ssh, 'sudo -S dpkg -i cuda-repo-ubuntu2004-12-1-local_12.1.1-530.30.02-1_amd64.deb', mypassword)
     execute_command(ssh, 'sudo -S cp /var/cudapo-ubuntu2004-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/', mypassword)
     execute_command(ssh, 'sudo -S apt-get -y update', mypassword)
-    execute_command(ssh, 'sudo -S apt-get -y install cuda', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S apt-get -y install cuda', mypassword)
     execute_command(ssh, 'sudo -S reboot', mypassword)
     ssh = connect_ssh(myip, int(portnumber), myusername, mypassword)
-    temp_dir = create_directory_on_partition(ssh, "/dev/nvme0n1p3", "temp", mypassword)
 
     execute_command(ssh, 'sudo -S apt update', mypassword)
-    execute_command(ssh, 'sudo -S apt install npm -y', mypassword, temp_dir)
-    execute_command(ssh, 'sudo -S npm install pm2 -', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S apt install npm -y', mypassword)
+    execute_command(ssh, 'sudo -S npm install pm2 -', mypassword)
     execute_command(ssh, 'sudo -S  -u endless env PATH=$PATH:/usr/local/cuda/bin CUDA_HOME=/usr/local/cuda pip install git+https://github.com/opentensor/cubit.git@v1.1.2', mypassword)
-    execute_command(ssh, 'sudo -S apt install python3', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S apt install python3', mypassword)
     execute_command(ssh, 'sudo -S apt update', mypassword)
-    execute_command(ssh, 'sudo -S apt install python3-pip -y', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -S apt install python3-pip -y', mypassword)
 
     execute_command(ssh, 'sudo -S apt update', mypassword)
     execute_command(ssh, 'git clone https://github.com/commune-ai/commune.git')
@@ -125,11 +101,11 @@ if __name__ == '__main__':
     file_name="torch-1.13.1-cp38-cp38-manylinux1_x86_64.whl"
     execute_command(ssh, f'[ ! -f "{file_name}" ] &&  sudo -S wget "{file_url}" || echo "File {file_name} already exists. Skipping download."', mypassword)
 
-    execute_command(ssh, 'pip install torch-1.13.1-cp38-cp38-manylinux1_x86_64.whl', temp_dir)
-    execute_command(ssh, 'pip install bittensor', temp_dir)
+    execute_command(ssh, 'pip install torch-1.13.1-cp38-cp38-manylinux1_x86_64.whl')
+    execute_command(ssh, 'pip install bittensor')
 
     execute_command(ssh, 'sudo -S apt update', mypassword)
-    execute_command(ssh, 'sudo -u ' + myusername + ' env PATH=$PATH:/usr/local/cuda/bin CUDA_HOME=/usr/local/cuda pip install git+https://github.com/GithubRealFan/simple.git', mypassword, temp_dir)
+    execute_command(ssh, 'sudo -u ' + myusername + ' env PATH=$PATH:/usr/local/cuda/bin CUDA_HOME=/usr/local/cuda pip install git+https://github.com/GithubRealFan/simple.git', mypassword)
     execute_command(ssh, 'sudo -S reboot', mypassword)
 
     ssh.close()
