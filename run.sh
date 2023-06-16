08519e303ec83ac2cec7c4b5b293df137e6f363d
real_fan <121934188+GithubRealFan@users.noreply.github.com>
2023-06-15 22:30:03 -0400

    updated alice.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for no in $(seq 0 99); 
-    do btcli register --subtensor.network finney --netuid 1 --wallet.name alice --wallet.hotkey $no --cuda.TPB 512 --cuda.update_interval 800000 --cuda.dev_id 0 1 2 3 4 5 6 7 --cuda.use_cuda  --logging.debug --no_prompt;
+    do btcli register --subtensor.network finney --netuid 1 --wallet.name alice --wallet.hotkey $no --cuda.TPB 512 --cuda.update_interval 1600000 --cuda.dev_id 0 1 2 3 4 5 6 7 --cuda.use_cuda  --logging.debug --no_prompt;
 done
